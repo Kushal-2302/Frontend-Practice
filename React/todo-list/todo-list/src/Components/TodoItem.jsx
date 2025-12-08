ebd271ad2deb9fb1475c66d5c5e641d1256f057b
kushal <kushals0223@gmail.com>
2025-12-08 11:41:23 +0530

    changes added to todo list

--- React/todo-list/todo-list/src/Components/TodoItem.jsx
+++ React/todo-list/todo-list/src/Components/TodoItem.jsx
@@ -3,9 +3,10 @@
 export default function TodoItem() {
     return (
         <div className="todo-item">
-            <span id="id">ID</span>
-            <span className="description">Sample Todo Item</span>
-            <input className="checkbox" type="checkbox" />
+            <span id="id">1.</span>
+            <span className="description">Sample Todo Item Lorem ipsum dolor sit amet.</span>
+            <input type="checkbox" />
+            <button>Delete</button>
         </div>
     )
 }
